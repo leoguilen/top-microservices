754994f7c554b2d60f015d52a796a633e082c56a
Leonardo Guilen <leonardo.guilen@outlook.com.br>
2021-03-05 18:48:42 -0300

    refact: refactoring startup

---
+++ services/TOP.IdentityService/src/TOP.IdentityService.Api/Configurations/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using TOP.IdentityService.Domain.Models;
+using TOP.IdentityService.WebApi.Filters;
+
+namespace TOP.IdentityService.Api.Configurations.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddControllersWithJsonOptions(this IServiceCollection services)
+        {
+            var jsonSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            JsonConvert.DefaultSettings = () => jsonSettings;
+
+            services.AddControllers(o =>
+                {
+                    o.Filters.Add<ContextFilter>();
+                    o.Filters.Add<ExceptionFilter>();
+                })
+                .ConfigureApiBehaviorOptions(o => o.InvalidModelStateResponseFactory = ctx =>
+                {
+                    var errors = ctx.ModelState.Keys
+                        .Select(x => new
+                        {
+                            Key = x,
+                            Values = ctx.ModelState[x],
+                            Error = ctx.ModelState[x]?.Errors == null ? null : string.Join("|", ctx.ModelState[x]?.Errors.Select(e => e.ErrorMessage))
+                        })
+                        .Select(x => new InnerError
+                        {
+                            Title = $"validation error on field {x.Key}".Replace(" Data.", " "),
+                            Detail = string.IsNullOrWhiteSpace(x.Error) ? "Must be a valid value" : x.Error,
+                            Status = ((int)HttpStatusCode.BadRequest).ToString()
+                        });
+
+                    return new BadRequestObjectResult(new Error { Errors = errors });
+                })
+                .AddJsonSerializerOptions()
+                .AddNewtonsoftJson(jsonOpt => 
+                {
+                    jsonOpt.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    jsonOpt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+            return services;
+        }
+
+        private static IMvcBuilder AddJsonSerializerOptions(this IMvcBuilder mvcBuilder)
+            => mvcBuilder.AddJsonOptions(opt => 
+            {
+                opt.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                opt.JsonSerializerOptions.IgnoreNullValues = true;
+            });
+    }
+}
--- services/TOP.IdentityService/src/TOP.IdentityService.Api/Startup.cs
+++ services/TOP.IdentityService/src/TOP.IdentityService.Api/Startup.cs
@@ -2,21 +2,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Linq;
-using System.Net;
-using TOP.IdentityService.Domain.Models;
+using TOP.IdentityService.Api.Configurations.Extensions;
 using TOP.IdentityService.Infra.CrossCutting.IoC.DependencyInjection;
+using TOP.IdentityService.WebApi.Configurations.Extensions;
 using TOP.IdentityService.WebApi.Configurations.Swagger;
-using TOP.IdentityService.WebApi.Filters;
 
 namespace TOP.IdentityService.WebApi
 {
@@ -31,41 +27,12 @@ namespace TOP.IdentityService.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var jsonSettings = new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-            };
-
-            JsonConvert.DefaultSettings = () => jsonSettings;
-
             services
                 .AddIoc(Configuration)
+                .AddControllersWithJsonOptions()
+                .AddResponseCompression()
+                .AddRouting()
                 .AddHealthChecks().Services
-                .AddControllers(o =>
-                {
-                    o.Filters.Add<ContextFilter>();
-                    o.Filters.Add<ExceptionFilter>();
-                })
-                .ConfigureApiBehaviorOptions(o => o.InvalidModelStateResponseFactory = ctx =>
-                {
-                    var errors = ctx.ModelState.Keys
-                        .Select(x => new
-                        {
-                            Key = x,
-                            Values = ctx.ModelState[x],
-                            Error = ctx.ModelState[x]?.Errors == null ? null : string.Join("|", ctx.ModelState[x]?.Errors.Select(e => e.ErrorMessage))
-                        })
-                        .Select(x => new InnerError
-                        {
-                            Title = $"validation error on field {x.Key}".Replace(" Data.", " "),
-                            Detail = string.IsNullOrWhiteSpace(x.Error) ? "Must be a valid value" : x.Error,
-                            Status = ((int)HttpStatusCode.BadRequest).ToString()
-                        });
-
-                    return new BadRequestObjectResult(new Error { Errors = errors });
-                })
-                .AddNewtonsoftJson().Services
                 .AddApiVersioning(o =>
                 {
                     o.ReportApiVersions = true;
@@ -77,8 +44,7 @@ namespace TOP.IdentityService.WebApi
                     o.SubstituteApiVersionInUrl = true;
                 })
                 .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>()
-                .AddSwaggerGen(o => { o.OperationFilter<SwaggerDefaultValues>(); })
-                .AddSwaggerGenNewtonsoftSupport();
+                .AddSwaggerGen(o => { o.OperationFilter<SwaggerDefaultValues>(); });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
